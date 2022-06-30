5a14f75565c12d149543c635802f113ff64a7735
kseniyaAvdeenko <ksunyaef@gmail.com>
2022-07-01 01:36:42 +0300

    first commit

---
+++ main/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
+
+def index(request):
+    tasks = Task.objects.order_by("-id")
+    return render(request, 'main/index.html', {"title": "Главная страница", 'tasks': tasks, 'Created_on': "Создано"} )
+
+def contacts(request):
+    return render(request, 'main/contacts.html' )
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def create(request):
+    error = ''
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+        else:
+            error = "Форма была не верной"    
+    form = TaskForm()
+    context = {"form": form, 'error': error}
+    return render(request, 'main/create.html', context)
+
+def delete(request):
+    tasks = Task.objects.all()
+    return render(request, 'main/delete.html', {'tasks': tasks,})
